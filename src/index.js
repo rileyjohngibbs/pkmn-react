d70e141503b225a2e3a816819611841fbf02eb5b
Riley Gibbs <rileyjohngibbs@gmail.com>
2019-01-02 18:10:42 -0800

    First iteration of the app
    - Displays Pokemon name and status (hp)
    - Pokemon can attack one another
    - Message with result of attack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Pokemon {
+  constructor(name) {
+    this.name = name;
+    this.hp = 100;
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  toHit(target) {
+    return Math.random() >= 0.1;
+  }
+
+  damage(target) {
+    return Math.ceil(Math.random() * 10);
+  }
+
+  takeDamage(damage) {
+    this.hp = Math.max(this.hp - damage, 0);
+  }
+
+  statusMessage() {
+    return (
+      this.hp === 0
+      ? this.name + ' has fainted!'
+      : this.name + ' has ' + this.hp + ' hit points.'
+    );
+  }
+}
+
+class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.first = new Pokemon(props.first);
+    this.second = new Pokemon(props.second);
+    this.state = {
+      first: this.first,
+      second: this.second,
+      combatMessage: 'Battle has started!',
+    }
+  }
+
+  handleAttack(attacker, target) {
+    const hit = attacker.toHit(target);
+    let combatMessage;
+    if (hit) {
+      const damage = attacker.damage(target);
+      target.takeDamage(damage);
+      combatMessage = `${attacker.name} attacked ${target.name} for ${damage} damage.`;
+    } else {
+      combatMessage = `${attacker.name} missed ${target.name}!`
+    }
+    this.setState({
+      first: this.first,
+      second: this.second,
+      combatMessage: combatMessage,
+    });
+  }
+
+  renderAttackButton(attacker, target) {
+    const valid = attacker.hp && target.hp;
+    return valid
+    ? (
+      <button className="move" onClick={() => this.handleAttack(attacker, target)}>
+        Attack
+      </button>
+    )
+    : (
+      <button className="move" disabled>Attack</button>
+    );
+  }
+
+  render() {
+    return (
+      <div className="combat">
+        <div>
+          <span className="pokemon-name">{this.state.first.name}</span><br />
+          {this.state.first.statusMessage()}<br />
+          {this.renderAttackButton(this.state.first, this.state.second)}
+        </div>
+        <div>
+          <span className="pokemon-name">{this.state.second.name}</span><br />
+          {this.state.second.statusMessage()}<br />
+          {this.renderAttackButton(this.state.second, this.state.first)}
+        </div>
+        <div>
+          {this.state.combatMessage}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Battle first="Charmander" second="Squirtle" />,
+  document.getElementById('root')
+);
